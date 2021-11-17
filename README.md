78e2c5457d37d63fa956502074aab66a366840e9
kk-im <kellykim0996@gmail.com>
2021-11-17 15:52:28 -0600

    metamask integration and readme

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Kelly Kim NFT Art/Performance Art Portfolio
+
+In order to divide my work and play, as well as add Web3 integrations into my NFT Dance portfolio,
+I created a website for my art.
+
+
+
+
+
+
+# Acknowledgements
+
+Big shoutout to Rik Lomas at SuperHi for an awesome Crypto for Beginners course.
+
+
+
+
+
+
+## Tech Stack
+
+**Client:** HTML5, Vanilla Javascript, CSS
+
+**Libraries:** web3.js, base.css
+
+**Server:** Heroku
+
+
+## Lessons Learned
+
+- How to connect my site to a Metamask wallet using their developers API
+- Testing, using the Ropstem test network & Ganache app
--- tip.js
+++ tip.js
@@ -1,14 +1,28 @@
-const form = document.querySelector("form")
+const web3 = new Web3(Web3.givenProvider)
 
-const send = function (amount) {
-  alert("We're sending this amount of money " + amount)
+
+const form = document.querySelector("form");
+
+const send = async function (amount) {
+  const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+
+  const wei = web3.utils.toWei(amount, "ether");
+
+  if (accounts.length > 0) {
+    window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [{
+          from: accounts[0],
+          to: "0x69d015C9Bd2DA888C75018b0279ec4748E03213D",
+          value: web3.utils.toHex(wei)
+        }],
+    });
+  }
 }
 
 if (window.ethereum) {
   form.classList.add("has-eth");
 }
-
-
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
